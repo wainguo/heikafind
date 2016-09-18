974093e0d719801add0fbbbe5fb98ad0f9c95d23
guoshengxing <guoshengxing@renrendai.com>
2016-09-18 15:20:30 +0800

    编码方式修改

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -177,8 +177,8 @@ class ArticleController extends Controller
 //            $params = http_build_query($query, null, '&', PHP_QUERY_RFC3986);
 //            $shareScheme = "heika://share?". $params;
 
-            $shareScheme = "heika://share?title=".urlencode($article->title).
-                "&shareDescription=".urlencode($article->description)."&url=".$articleUrl."&imgUrl=".$coverUrl;
+            $shareScheme = "heika://share?title=".$article->title.
+                "&shareDescription=".$article->description."&url=".$articleUrl."&imgUrl=".$coverUrl;
 
 //            $article->shareScheme =urlencode($shareScheme);
 //            $article->shareScheme = htmlentities($shareScheme, ENT_QUOTES, "UTF-8");
