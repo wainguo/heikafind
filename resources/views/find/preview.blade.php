dcecb36cfb214eeb8805044443403ab752ea5d22
guoshengxing <guoshengxing@renrendai.com>
2016-09-19 19:36:06 +0800

    打开app方式修改

--- resources/views/find/preview.blade.php
+++ resources/views/find/preview.blade.php
@@ -82,19 +82,63 @@
         }
 
         //打开app
-        function openApp(appDetailUrl){
-            var loadDateTime = new Date();
-            window.setTimeout(function() {
-                var timeOutDateTime = new Date();
-                if (timeOutDateTime - loadDateTime > 5000) {
-                    if(isiOS){
-                        window.location = appStore;
-                    }else{
-                        window.location = yingyongbao;
+//        function openApp(appDetailUrl){
+//            var loadDateTime = new Date();
+//            window.setTimeout(function() {
+//                var timeOutDateTime = new Date();
+//                if (timeOutDateTime - loadDateTime > 5000) {
+//                    if(isiOS){
+//                        window.location = appStore;
+//                    }else{
+//                        window.location = yingyongbao;
+//                    }
+//                }
+//            },25);
+//            window.location = appDetailUrl;
+//        }
+
+        function openApp(openUrl, callback) {
+            //检查app是否打开
+            function checkOpen(cb){
+                var _clickTime = +(new Date());
+                function check(elsTime) {
+                    if ( elsTime > 3000 || document.hidden || document.webkitHidden) {
+                        cb(1);
+                    } else {
+                        cb(0);
                     }
                 }
-            },25);
-            window.location = appDetailUrl;
+                //启动间隔20ms运行的定时器，并检测累计消耗时间是否超过3000ms，超过则结束
+                var _count = 0, intHandle;
+                intHandle = setInterval(function(){
+                    _count++;
+                    var elsTime = +(new Date()) - _clickTime;
+                    if (_count>=100 || elsTime > 3000 ) {
+                        clearInterval(intHandle);
+                        check(elsTime);
+                    }
+                }, 20);
+            }
+
+            //在iframe 中打开APP
+            var ifr = document.createElement('iframe');
+            ifr.src = openUrl;
+            ifr.style.display = 'none';
+            if (callback) {
+                checkOpen(function(opened){
+                    callback && callback(opened);
+                });
+            }
+
+            document.body.appendChild(ifr);
+            setTimeout(function() {
+                document.body.removeChild(ifr);
+            }, 2000);
+        }
+
+        function logOpen(opened) {
+            console.log("opened:"+opened);
+            alert(opened);
         }
 
         $('.once-use').click(function(){
@@ -109,7 +153,8 @@
                     }
                 });
             }else{
-                openApp(scheme);
+//                openApp(scheme);
+                openApp(scheme, logOpen);
             }
         });
 
