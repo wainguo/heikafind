6f1fea95125b20df733f056acc8142c2359bbd1b
guoshengxing <guoshengxing@renrendai.com>
2016-09-22 14:55:05 +0800

    内容左右边距

--- resources/views/find/preview.blade.php
+++ resources/views/find/preview.blade.php
@@ -29,7 +29,7 @@
                     </div>
                 </div>
                 <p><span>{{$article->description}}</span></p>
-                <div>
+                <div class="has-distance">
                     {!! $article->content !!}
                 </div>
             </div>
